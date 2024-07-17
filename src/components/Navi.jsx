34c1f25c6a3347644ccbc3a8a1af9227f78d1420
darrellabuguen <darrellabuguen2@gmail.com>
2024-07-17 13:49:37 +0800

    fix the category in navi

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -15,15 +15,16 @@ const Navi = () => {
     return (
         <>
             <header>
-                <nav className='flex items-center'>
-                    <Link to={"/"}
-                        className="logo text-2xl flex-1"
-                        style={{
-                            textShadow: "1px 1px 3px #323232"
-                        }}
-                    >
-                        Recipe Realm
-                    </Link>
+                <nav className='flex items-center font-medium'>
+                    <div className="logo text-2xl flex-1">
+                        <Link to={"/"}
+                            style={{
+                                textShadow: "1px 1px 3px #323232"
+                            }}
+                        >
+                            Recipe Realm
+                        </Link>
+                    </div>
                     <div className='flex items-center gap-6 flex-1'>
                         <Link to="/"
                             className="hover:text-red-500"
@@ -31,7 +32,7 @@ const Navi = () => {
                             Home
                         </Link>
                         <div
-                            className='relative hover:text-red-500'
+                            className='relative hover:text-red-500 z-10 cursor-pointer'
                             onMouseOver={() => {
                                 let categories = document.querySelector(".category-container");
                                 isHovered(`${categories.scrollHeight}px`);
@@ -42,12 +43,12 @@ const Navi = () => {
                         >
                             Categories
                             <div
-                                className='absolute left-0 overflow-hidden rounded-md shadow-lg category-container transition-all delay-75'
+                                className='absolute bg-slate-100 left-0 font-normal overflow-hidden rounded-md shadow-xl category-container transition-all delay-75'
                                 style={{
                                     maxHeight: `${hovered}`
                                 }}
                             >
-                                <div className='border overflow-hidden rounded-md items-center flex flex-col w-60'>
+                                <div className='overflow-hidden rounded-md items-center flex flex-col w-60'>
                                     {categories.map((category) => {
                                         return (
                                             <Link to={`/category/${category.name}/0`}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,7 +7,7 @@ const Search = () => {
     return (
         <div className='my-4 flex justify-center gap-1'>
             <input type="search"
-                className='border outline-none rounded-md bg-gray-200 pl-1 focus:border-gray-400 transition delay-75 ease-in-out'
+                className='border-2 outline-none rounded-md pl-1 focus:border-red-500 transition delay-75 ease-in-out'
                 autoComplete='off'
                 placeholder='search'
                 id='search'
@@ -18,13 +18,13 @@ const Search = () => {
                 }}
             />
             <button
-                className='p-2 bg-gray-200 outline-none rounded-md hover:bg-gray-400 transition delay-75 ease-in-out'
+                className=' outline-none rounded-md transition delay-75 ease-in-out'
                 onClick={() => {
                     const query = document.querySelector('input[type="search"]').value;
                     navigate(`/search/${query}/0`)
                 }}
             >
-                <IoSearch />
+                <IoSearch className='hover:text-red-500' />
             </button>
         </div>
     )
