0ea8617e949c2ae1665bdd60d3921627a79ce14a
darrellabuguen <darrellabuguen2@gmail.com>
2024-08-01 15:57:05 +0800

    made the navigation responsive

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Search from '../components/Search';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiArrowDownSLine } from "react-icons/ri";
 
 const Navi = () => {
     const categories = [
@@ -12,24 +14,26 @@ const Navi = () => {
         { name: "Soup" }
     ]
     const [hovered, isHovered] = useState("0px");
+    const [showMenu, setMenu] = useState("0px");
+    const [rotateIcon, setRotation] = useState("rotate-0");
 
     return (
         <>
-            <header className="mb-14">
+            <header className="mb-14 max-md:mb-4">
                 <nav className='flex items-center font-medium'>
-                    <div className="logo text-4xl flex-1">
+                    <div className="logo text-4xl flex-1 max-lg:text-3xl">
                         <Link to={"/"}>
                             Recipe Realm
                         </Link>
                     </div>
-                    <div className='flex items-center gap-6'>
+                    <div className='flex items-center gap-6 max-md:hidden'>
                         <Link to="/"
                             className="hover:text-red-500"
                         >
                             Home
                         </Link>
                         <div
-                            className='relative hover:text-red-500 z-10 cursor-pointer'
+                            className='relative hover:text-red-500 z-10 cursor-pointer max-md:hidden'
                             onMouseOver={() => {
                                 let categories = document.querySelector(".category-container");
                                 isHovered(`${categories.scrollHeight}px`);
@@ -66,8 +70,76 @@ const Navi = () => {
                         </Link>
                         <Search />
                     </div>
+                    <div
+                        className="hidden cursor-pointer max-md:flex max-md:gap-2"
+                        onClick={() => {
+                            let mbc = document.querySelector(".mobile-container");
+                            setMenu(showMenu === "0px" ? `${mbc.scrollHeight}px` : "0px");
+                            isHovered("0px");
+                            setRotation("rotate-0")
+                        }}
+                    >
+                        <Search />
+                        <GiHamburgerMenu className="h-5 w-5" />
+                    </div>
                 </nav>
             </header>
+            <div
+                className="mobile-container ml-2 z-10 flex flex-col gap-4 font-medium max-md:mb-10 overflow-hidden transition-all duration-300 ease-linear"
+                style={{
+                    maxHeight: `${showMenu}`
+                }}
+            >
+                <Link to="/"
+                    className="hover:text-red-500"
+                >
+                    Home
+                </Link>
+                <div
+                    className='hover:text-red-500 z-10 cursor-pointer'
+                    onClick={() => {
+                        if (hovered == "0px") {
+                            let categories = document.querySelector(".mbcategory-container");
+                            let mbc = document.querySelector(".mobile-container");
+                            isHovered(`${categories.scrollHeight}px`);
+                            setMenu(`${mbc.scrollHeight + categories.scrollHeight}px`);
+                            setRotation("-rotate-180")
+                        } else {
+                            isHovered("0px");
+                            setRotation("rotate-0")
+                        }
+                    }}
+                >
+                    <div className="flex items-center">
+                        Categories
+                        <RiArrowDownSLine className={`h-5 w-5 ${rotateIcon} transition-all duration-200 ease-linear`} />
+                    </div>
+                    <div
+                        className='bg-slate-100 font-normal overflow-hidden rounded-md shadow-xl mbcategory-container transition-all duration-200 ease-linear'
+                        style={{
+                            maxHeight: `${hovered}`
+                        }}
+                    >
+                        <div className='overflow-hidden rounded-md z-20 items-center flex flex-col'>
+                            {categories.map((category) => {
+                                return (
+                                    <Link to={`/category/${category.name}/0`}
+                                        key={category.name}
+                                        className='transition p-4 text-black duration-200 ease-in-out hover:bg-red-500 hover:text-white w-full'
+                                    >
+                                        {category.name}
+                                    </Link>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <Link to={"/cuisines"}
+                    className="hover:text-red-500"
+                >
+                    Cuisines
+                </Link>
+            </div>
         </>
     )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearch } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
     const navigate = useNavigate();
+    const [isVisible, setVisibility] = useState("hidden");
+    const [isInFront, setZIndex] = useState("z-0");
     return (
         <div className='flex justify-center gap-1'>
-            <input type="search"
-                className='border-2 border-black outline-none rounded-md pl-1 focus:border-red-500 transition delay-75 ease-in-out'
-                autoComplete='off'
-                placeholder='search'
-                id='search'
-                onKeyDown={(e) => {
-                    if (e.key == "Enter" && e.target.value.length > 0) {
-                        navigate(`/search/${e.target.value}/0`);
-                    }
+            <div className={`absolute top-0 left-0 w-full h-16 ${isVisible} ${isInFront}`}
+                onFocus={() => {
+                    setVisibility("block");
+                    setZIndex("z-20");
                 }}
-            />
+
+                onBlur={() => {
+                    setVisibility("hidden");
+                    setZIndex("z-0");
+                }}
+            >
+                <input type="search"
+                    className='w-full h-full outline-none pl-1 p-2 bg-slate-100 border-b-2 focus:border-red-500'
+                    autoComplete='off'
+                    placeholder='search'
+                    id='search'
+                    onKeyDown={(e) => {
+                        if (e.key == "Enter" && e.target.value.length > 0) {
+                            navigate(`/search/${e.target.value}/0`);
+                        }
+                    }}
+                />
+            </div>
             <button
                 className=' outline-none rounded-md transition delay-75 ease-in-out hover:text-red-500'
                 onClick={() => {
-                    const query = document.querySelector('input[type="search"]').value;
-                    navigate(`/search/${query}/0`)
+                    setVisibility(isVisible == "hidden" ? "block" : "hidden");
+                    setZIndex(isInFront == "z-0" ? "z-20" : "z-0");
                 }}
             >
                 <IoSearch className='w-5 h-5' />
